c42ce8d11ff3d446436a1a189a4dcb240837b7f9
osvaldo <joseconstantino987@gmail.com>
2026-07-06 21:07:15 -0600

    Arregle bug y elimine los folders innecesarios

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_learningstylesurvey';
-$plugin->version   = 2026042603; // Versión del plugin, si se incrementa forza el update
+$plugin->version   = 2026070701; // Versión del plugin, si se incrementa forza el update
 $plugin->requires  = 2022041900; // Moodle 4.0 o superior
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0';
